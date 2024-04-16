28c65f20eb792fbe271a07967c5c8ba0041b22bb
Kevin Harismendy <kharismendy@veepee.com>
2024-04-16 16:25:35 +0200

    refactor(worker): get min guild id job
    refactor code to be modular and allow job to have multiple entry. Server id and subregion

--- Workers/Jobs/FindMinGuildIdServerJob.cs
+++ Workers/Jobs/FindMinGuildIdServerJob.cs
@@ -1,13 +1,15 @@
 using Core.Services;
 using Core.Services.Models;
 using Entities.Context;
+using Entities.Models;
 using Hangfire;
+using Hangfire.Console;
 using Hangfire.Server;
 using Lom.Hangfire;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using PuppeteerSharp;
 using System.Text.Json;
+using Server = Entities.Models.Server;
 
 namespace Jobs.Jobs;
 
@@ -18,37 +20,73 @@ namespace Jobs.Jobs;
 #endif
 public class FindMinGuildIdServerJob(LomDbContext lomDbContext, BrowserService browserService, ILogger<FindMinGuildIdServerJob> logger)
 {
-    private bool _guildFound = false;
+    private bool _guildFound;
     private long _minGuildId;
+
+    [JobDisplayName("FindMinGuildIdServerJob : {1}")]
+    [AutomaticRetry(Attempts = WorkerConstants.TotalRetry, OnAttemptsExceeded = AttemptsExceededAction.Fail)]
+    public async Task ExecuteAsync(PerformContext context, ServerShortName subRegion, CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation("Starting find min guild id job for {SubRegion}", subRegion);
+        var subRegionServers = await lomDbContext.Servers.OrderBy(x => x.ServerId).Where(x => x.ShortName == subRegion).ToListAsync(cancellationToken: cancellationToken);
+        if (subRegionServers.Count == 0)
+        {
+            logger.LogError("No servers with subregion {SubRegion} found", subRegion);
+            return;
+        }
+        await PreparePuppeteer(cancellationToken);
+        var progress = context.WriteProgressBar();
+        foreach (var server in subRegionServers.WithProgress(progress))
+        {
+            await FindMinGuildIdServer(server, cancellationToken);
+        }
+        logger.LogInformation("Finished scrapping guild id for {SubRegion}", subRegion);
+    }
 
     [JobDisplayName("FindMinGuildIdServerJob : {1}")]
     [AutomaticRetry(Attempts = WorkerConstants.TotalRetry, OnAttemptsExceeded = AttemptsExceededAction.Fail)]
     public async Task ExecuteAsync(PerformContext context, int serverId, CancellationToken cancellationToken = default)
     {
-        logger.LogInformation("Starting find min guild id job");
+        logger.LogInformation("Starting find min guild id job for server {ServerId}", serverId);
         var server = await lomDbContext.Servers.FirstOrDefaultAsync(x => x.ServerId == serverId, cancellationToken: cancellationToken);
-        var previousServer = await lomDbContext.Servers.FirstOrDefaultAsync(x => x.ServerId == serverId - 1, cancellationToken: cancellationToken);
         if (server is null)
         {
             logger.LogError("Server with id {ServerId} not found", serverId);
             return;
         }
+        await PreparePuppeteer(cancellationToken);
+        await FindMinGuildIdServer(server, cancellationToken);
+    }
+
+
+    private async Task FindMinGuildIdServer(Server server, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Starting find min guild id job for server {ServerId}", server.ServerId);
+        var previousServer = await lomDbContext.Servers.FirstOrDefaultAsync(x => x.ServerId == server.ServerId - 1, cancellationToken: cancellationToken);
+        if (previousServer is not null && server.MinGuildId is not null)
+        {
+            logger.LogInformation("Server {ServerId} already has min guild id", server.ServerId);
+            return;
+        }
         _minGuildId = previousServer is null ? server.MinGuildId!.Value : ExtrapolateBeginGuildId(previousServer.MinGuildId!.Value);
-        //Wait for the browser to be ready
-        await Task.Delay(8000, cancellationToken);
-        await browserService.ChangePrintLevel();
-        //Hook to page console
-        browserService.ConsoleMessageEvent += async (sender, e) => await ConsoleMessageReceived(sender, e);
-        var maxGuildId = _minGuildId + 20000; 
+        var maxGuildId = _minGuildId + 20000;
         while (!_guildFound && _minGuildId < maxGuildId)
         {
             await browserService.WriteToConsole($"netManager.send(\"guild.guild_members_info_c2s\", {{ guild_id: {_minGuildId}, source: undefined }}, false);");
             _minGuildId += 10;
             await Task.Delay(100, cancellationToken);
         }
-        server!.MinGuildId = _minGuildId - 100;
+        server.MinGuildId = _minGuildId - 50;
         await lomDbContext.SaveChangesAsync(cancellationToken);
-        logger.LogInformation("Finished Lom scrapper job");
+        logger.LogInformation("Finished scrapping guild id for server {ServerId}", server.ServerId);
+    }
+
+    private async Task PreparePuppeteer(CancellationToken cancellationToken)
+    {
+        await Task.Delay(8000, cancellationToken);
+        await browserService.ChangePrintLevel();
+        //Hook to page console
+        browserService.ConsoleMessageEvent += async (sender, e) => await ConsoleMessageReceived(sender, e);
     }
 
     private static long ExtrapolateBeginGuildId(long previousServerMinGuildId)
@@ -56,19 +94,14 @@ public class FindMinGuildIdServerJob(LomDbContext lomDbContext, BrowserService b
         var guildId = previousServerMinGuildId + 134210000;
         return guildId - guildId % 10000;
     }
-
-
+    
     private async Task ConsoleMessageReceived(object? sender, ConsoleMessageEvent e)
     {
         var message = e.Message.Split(' ');
         if (message.Length < 3) return;
         switch (message[2])
         {
-            case "guild.guild_info_s2c":
-                logger.LogInformation("Guild info received");
-                break;
             case "guild.guild_members_info_s2c":
-                logger.LogInformation("Guild members info received");
                 var jsonValue = await e.Response!.JsonValueAsync();
                 var stringValue = jsonValue.ToString();
                 var guildMemberInfos = JsonSerializer.Deserialize<GuildMembersInfoResponse>(stringValue!);
